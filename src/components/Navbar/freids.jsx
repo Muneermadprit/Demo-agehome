274d8b06e7eee568844bde10ed65da7c51f24ac8
Muneer A <137588707+Muneermadprit@users.noreply.github.com>
2024-08-06 10:27:45 +0530

    first commit

--- src/components/Navbar/freids.jsx
+++ src/components/Navbar/freids.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
-import image1 from'../../assets/friends (1).png'
-import image2 from '../../assets/family.png'
-import image3 from '../../assets/charity.png'
+import image1 from '../../assets/friends (1).png';
+import image2 from '../../assets/family.png';
+import image3 from '../../assets/charity.png';
 import Topproducts from './topproducts';
 
 function Friends() {
   return (
-    <div>
-      <h1 className='sm:mt-[200px] sm:w-full sm:h-[200px] flex justify-center text-sm items-center sm:text-[40px]   font-bold text-gray-500 mr-40'>Why choose Radfield?</h1>
-      <div className='grid sm:grid-cols-3 sm:w-full sm:h-[500px] sm:p-20 w-[300px] mr-100 pr-10 ' >
-        <div className='flex flex-col items-center sm:text-[25px] text-10 font-bold text-gray-500'>
-          <img src={image2} className='w-40 h-40'/>
+    <div className='sm:mt-[200px]'>
+      <h1 className='text-center text-sm sm:text-[40px] font-bold text-gray-500 sm:mb-10 mb-5'>
+        Why choose Radfield?
+      </h1>
+      <div className='grid grid-cols-1 sm:grid-cols-3 gap-10 sm:w-full sm:px-20 px-5'>
+        <div className='flex flex-col items-center text-center sm:text-[25px] text-[15px] font-bold text-gray-500'>
+          <img src={image2} className='w-40 h-40 mb-4'/>
           We are Family
-          <p className='text-sm font-normal p-4'>we are treated as our careers best family and
-          we empower them to treate our clients with the same warm and respect</p>
+          <p className='text-sm font-normal p-4'>
+            We are treated as our carers' best family and
+            we empower them to treat our clients with the same warmth and respect.
+          </p>
         </div>
-        <div className='flex flex-col items-center sm:text-[25px] font-bold text-gray-500'>
-        <img src={image1} className='w-40 h-40'/>
+        <div className='flex flex-col items-center text-center sm:text-[25px] text-[15px] font-bold text-gray-500'>
+          <img src={image1} className='w-40 h-40 mb-4'/>
           We are on your side
-          <p className='text-sm font-normal p-4'>we are treated as our careers best family and
-          we empower them to treate our clients with the same warm and respect</p>
+          <p className='text-sm font-normal p-4'>
+            We are treated as our carers' best family and
+            we empower them to treat our clients with the same warmth and respect.
+          </p>
         </div>
-        <div className='flex flex-col items-center sm:text-[25px] font-bold text-gray-500'>
-        <img src={image3} className='w-40 h-40'/>
+        <div className='flex flex-col items-center text-center sm:text-[25px] text-[15px] font-bold text-gray-500'>
+          <img src={image3} className='w-40 h-40 mb-4'/>
           We are proud to care
-          <p className='text-sm font-normal p-4'>we are treated as our careers best family and
-          we empower them to treate our clients with the same warm and respect</p>
+          <p className='text-sm font-normal p-4'>
+            We are treated as our carers' best family and
+            we empower them to treat our clients with the same warmth and respect.
+          </p>
         </div>
-     
       </div>
-    <div className='mr-20'> <Topproducts/> </div>
+      <div className='mt-10'>
+        <Topproducts />
+      </div>
     </div>
   );
 }
